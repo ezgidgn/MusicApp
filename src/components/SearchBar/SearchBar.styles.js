20d90d3195f53ce369861653783c743f457458f3
Ezgi <ezgidogan9983@gmail.com>
2023-03-03 14:32:34 +0300

    first commit

---
+++ src/components/SearchBar/SearchBar.styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        backgroundColor:'#eceff1',
+        padding:5,
+        margin:2,
+        borderRadius:10,
+        height:50,
+
+    }
+})
